ddc2822c24ad512f3a2eebbd275b9acb398a2d85
muhammadranju <mdranju23@gmail.com>
2022-06-21 21:50:41 +0600

    new features added

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 let fromData = document.getElementById("fromData");
 
+const x = document.getElementById("demo");
+
+function getLocation() {
+   if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+   } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+   }
+}
+
+function showPosition(position) {
+   x.innerHTML =
+      "Latitude: " +
+      position.coords.latitude +
+      "<br>Longitude: " +
+      position.coords.longitude;
+}
+
 fromData.addEventListener("submit", async function (e) {
    e.preventDefault();
 
